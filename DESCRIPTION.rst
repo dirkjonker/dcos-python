159d6ebef2d60b55c434049a15d282474e65a7c5
Dirk Jonker <dirk.jonker@lunatech.com>
2018-05-30 13:56:57 +0200

    Update package details

--- DESCRIPTION.rst
+++ DESCRIPTION.rst
@@ -1,11 +1,6 @@
-DC/OS Command Line Interface
-============================
-The DC/OS Command Line Interface (CLI) is a command line utility that
-provides a user-friendly yet powerful way to manage DC/OS installations.
+DC/OS Libraries for Python
+==========================
 
 This project is open source. Please see GitHub_ to access source code and to contribute.
 
-Full documentation is available for the DC/OS CLI on the `DC/OS docs website`_.
-
-.. _GitHub: https://github.com/mesosphere/dcos-cli
-.. _DC/OS docs website: https://dcos.io/docs/usage/cli/
+.. _GitHub: https://github.com/dirkjonker/dcos-python
--- setup.py
+++ setup.py
@@ -12,23 +12,21 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='dcos',
+    name='dcos-python',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
     version=dcos.version,
 
-    description='DCOS Common Modules',
+    description='DCOS Python Libraries',
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/mesosphere/dcos-cli',
+    url='https://github.com/dirkjonker/dcos-python',
 
     # Author details
-    author='Mesosphere, Inc.',
-    author_email='help@dcos.io',
-
+    author='Dirk Jonker',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -48,8 +46,10 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     # What does your project relate to?
